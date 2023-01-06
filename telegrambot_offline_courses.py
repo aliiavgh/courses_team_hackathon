8e8d8e7b2193102bc3f3888db2b1a1f6b0c46829
Hello <musabekova.amina13@gmail.com>
2023-01-06 18:50:27 +0600

    info

--- telegrambot_offline_courses.py
+++ telegrambot_offline_courses.py
@@ -1,29 +1,50 @@
 import json
+
+import peewee
 import telebot
 from telebot import types
 from test import *
+from info import *
 
-URL = 'https://www.google.com/maps/search/%D1%8F%D0%B7%D1%8B%D0%BA%D0%BE%D0%B2%D1%8B%D0%B5+%D0%BA%D1%83%D1%80%D1%81%D1%8B+%D0%B2+%D0%91%D0%B8%D1%88%D0%BA%D0%B5%D0%BA%D0%B5/@42.8760111,74.5856421,14z/data=!3m1!4b1'
+URL = url
 
-bot = telebot.TeleBot('5876023372:AAHBK5lmPJ-wPxsV3dgE0b6Kqcpr5uxa6UM')
-choice = types.InlineKeyboardMarkup()
-button = types.InlineKeyboardButton('Courses', callback_data='courses')
-choice.add(button)
+bot = telebot.TeleBot(TOKEN)
 
 html = get_courses(url=URL)
 soup = get_soup(html)
 data = get_data(soup)
 
+courses = peewee.PostgresqlDatabase(
+    'team1',
+    user=USER,
+    password=PASSWORD,
+    host='localhost',
+    port=5432
+)
+
+cursor = courses.cursor()
+
+cursor.execute('SELECT title, status, price FROM courses_course')
+results = cursor.fetchall()
+
 
 @bot.message_handler(commands=['start'])
-def star_message(message):
-    bot.send_message(message.chat.id, 'Hello')
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Off')
+    btn2 = types.KeyboardButton('On')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, text='Hello', reply_markup=markup)
 
 
-@bot.message_handler(commands=['offline'])
-def get_offline_courses(message):
-    for i in data:
-        bot.send_message(message.chat.id, i)
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if message.text == 'Off':
+        for i in data[:5]:
+            bot.send_message(message.chat.id, i)
+    elif message.text == 'On':
+        for k in results[:5]:
+            bot.send_message(message.chat.id, k)
 
 
 bot.polling()
